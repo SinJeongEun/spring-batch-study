5b6efe6d8cd5e8863bf7397654d9cbc19ef1782b
SinJeongEun <lisasje25@gmail.com>
2022-07-28 02:04:40 +0900

    #8 JdbcBatchItemWriter

--- src/main/java/com/example/springbatchexample/part3/ItemWriterConfiguration.java
+++ src/main/java/com/example/springbatchexample/part3/ItemWriterConfiguration.java
@@ -1,22 +1,31 @@
 package com.example.springbatchexample.part3;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,21 +35,33 @@ public class ItemWriterConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    public ItemWriterConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-        this.jobBuilderFactory = jobBuilderFactory;
-        this.stepBuilderFactory = stepBuilderFactory;
+    @Qualifier("datasource")
+    private  DataSource dataSource;
+
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource DataSource() {
+       return DataSourceBuilder.create().build();
     }
+    public ItemWriterConfiguration(JobBuilderFactory jobBuilderFactory,
+                                       StepBuilderFactory stepBuilderFactory
+//                                       DataSource dataSource
+    ) {
+            this.jobBuilderFactory = jobBuilderFactory;
+            this.stepBuilderFactory = stepBuilderFactory;
+//            this.dataSource = dataSource;
+        }
 
     @Bean
     public Job itemWriterJob() throws Exception {
         return jobBuilderFactory.get("itemWriterJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.csvItemWriterStep())
+                .next(this.jdbcBatchItemWriterStep())
                 .build();
     }
 
     @Bean
-    @JobScope
     public Step csvItemWriterStep() throws Exception {
 
         return stepBuilderFactory.get("csvItemWriterStep")
@@ -48,6 +69,25 @@ public class ItemWriterConfiguration {
                 .reader(itemReader())
                 .writer(csvItemWriter())
                 .build();
+    }
+
+    @Bean
+    public Step jdbcBatchItemWriterStep() {
+        return stepBuilderFactory.get("jdbcBatchItemWriterStep")
+                .<Person, Person>chunk(10)
+                .reader(itemReader())
+                .writer(jdbcBatchItemWriter())
+                .build();
+    }
+
+    private ItemWriter<Person> jdbcBatchItemWriter() {
+        JdbcBatchItemWriter<Person> itemWriter = new JdbcBatchItemWriterBuilder<Person>()
+                .dataSource(dataSource)
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>()) // BeanPropertyItemSqlParameterSourceProvider : person 객체를 파라미터로 자동생성 할 수 있는 기능
+                .sql("insert into person(name, age, address) values(:name, :age, :address)")
+                .build();
+        itemWriter.afterPropertiesSet();
+        return itemWriter;
     }
 
     private ItemWriter<Person> csvItemWriter() throws Exception {
