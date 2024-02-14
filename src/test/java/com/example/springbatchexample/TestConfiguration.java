271b5b297345a254067c830ffddd336dc4d10603
SinJeongEun <lisasje25@gmail.com>
2024-02-14 22:42:19 +0900

    테스트코드

--- src/test/java/com/example/springbatchexample/TestConfiguration.java
+++ src/test/java/com/example/springbatchexample/TestConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableBatchProcessing
+@EnableBatchProcessing      // 배치 작업 관리를 위한 JobBuilderFactory, StepBuilderFactory 등의 빈 자동 구성
 @EnableAutoConfiguration
 public class TestConfiguration {
 
--- src/test/java/com/example/springbatchexample/part3/SavePersonConfigurationTest.java
+++ src/test/java/com/example/springbatchexample/part3/SavePersonConfigurationTest.java
@@ -2,7 +2,9 @@ package com.example.springbatchexample.part3;
 
 import com.example.springbatchexample.TestConfiguration;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
@@ -16,7 +18,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBatchTest
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {SavePersonConfiguration.class, TestConfiguration.class })
 public class SavePersonConfigurationTest {
@@ -24,8 +25,17 @@ public class SavePersonConfigurationTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Before
+    public void resetPerson(){
+        personRepository.deleteAll();
+    }
+
+    @DisplayName("person 중복 insert 불가")
     @Test
-    public void test_allow_duplicate() throws Exception {
+    public void test_not_allow_duplicate() throws Exception {
         // job parameters 설정
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("allow_duplicate", "false")
@@ -38,6 +48,38 @@ public class SavePersonConfigurationTest {
         Assertions.assertThat(jobExecution.getStepExecutions().stream() // 구조에 따라 N개의 StepExecution 리턴된다.
                 .mapToInt(StepExecution::getWriteCount)
                 .sum())
+                .isEqualTo(personRepository.count())
+                .isEqualTo(3);
+    }
+
+    @DisplayName("person 중복 insert 허용")
+    @Test
+    public void test_allow_duplicate() throws Exception {
+        // job parameters 설정
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("allow_duplicate", "true")
+                .toJobParameters();
+
+        // SavePersonConfiguration.class 에서 실행된 job 실행결과가 리턴된다.
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // 검증
+        Assertions.assertThat(jobExecution.getStepExecutions().stream() // 구조에 따라 N개의 StepExecution 리턴된다.
+                        .mapToInt(StepExecution::getWriteCount)
+                        .sum())
+                .isEqualTo(personRepository.count())
+                .isEqualTo(100);
+    }
+
+    @DisplayName("Step 단위 테스트 (launchStep)")
+    @Test
+    public void test_step(){
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("savePersonStep");
+
+        Assertions.assertThat(jobExecution.getStepExecutions().stream()
+                .mapToInt(StepExecution::getWriteCount)
+                .sum())
+                .isEqualTo(personRepository.count())
                 .isEqualTo(3);
     }
 }
